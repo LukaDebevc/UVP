25ebd3618e0004201b3958bded9cad47dab2e24a
LukaDebevc <lukadebevc2002@gmail.com>
2022-07-01 17:06:10 +0200

    Oris igre

---
+++ igra.py
@@ -0,0 +1,13 @@
+# Pravila igre:
+# igra se na 2n * 2n plosci, v vsako polje gre natanko en kamen,
+# Igrata dva igralca (beli in crn), ki se izmenjujeta,
+# Vsako potezo igralec polozi 1 kamen svoje barve in 1 kamen sive barve,
+# Izjema sta 1. in 2. poteza
+# Pri 1. potezi beli polozi iskljucno kamen svoje barve
+# Pri 2. potezi crn poleg kamna svoje barve polozi tut 2 siva kamna.
+
+# Zmaga tisti igralec, ki ima vec povezav,
+# povezavo tvorijo beli in crni kamni, sivi pa ne
+# povezavo tvorijo kamni iste barve
+# povezave se stejeo v ravnih linijah (po vrsticah, stolpcih in diagonalah)
+# n kamnov v ravni liniji lastniku prenesejo n(n-1)/2 povezav
